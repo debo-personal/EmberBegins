33b80803a006257e182655d6bd09d933adb92af7
Debajit Majumder <debajit.business@gmail.com>
2013-07-31 00:10:56 +0530

    BabySteps added

---
+++ BabySteps/js/app.js
@@ -0,0 +1,11 @@
+var StartupApp= Ember.Application.create();
+
+StartupApp.IndexController= Ember.Controller.extend({
+firstProp: "It is the first property",
+secondProp: "It is the second property"
+});
+
+// Our model
+
+
+// Our Route
