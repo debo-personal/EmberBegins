1f333ffedef9ac2f7979a5379a834a0f8c20e21c
Debajit Majumder <debajit.business@gmail.com>
2013-08-01 00:37:15 +0530

    Basic Router implemented

--- BabySteps/js/app.js
+++ BabySteps/js/app.js
@@ -1,11 +1,17 @@
 var StartupApp= Ember.Application.create();
 
-StartupApp.IndexController= Ember.Controller.extend({
+/*StartupApp.IndexController= Ember.Controller.extend({
 firstProp: "It is the first property",
 secondProp: "It is the second property"
 });
-
+*/
 // Our model
 
 
 // Our Route
+
+StartupApp.IndexRoute= Ember.Route.extend({
+setupController: function(controller){
+	controller.set('roommates',['Manoj','Vishal','Sunny','Vinay']);
+}
+});
